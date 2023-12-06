a5d5495e4402428859661d48a4f37462c7ab7949
Paolo Boldi <paolo.boldi@unimi.it>
2023-12-06 10:21:22 +0100

    Aggiunti vari metodi

--- mline/tutto.go
+++ mline/tutto.go
@@ -2,6 +2,8 @@ package mline
 
 import (
   "github.com/boldip/geom/mpoint"
+  "fmt"
+  "math"
 )
 
 /* A (straight) line specified by the equation y=mx+q */
@@ -20,4 +22,21 @@ func (r1 Line) IsParallel(r2 Line) bool {
 
 func (r Line) Belongs(p mpoint.Point) bool {
   return p.Y() == r.m * p.X() + r.q
+}
+
+func (r Line) String() string {
+  return fmt.Sprintf("y=%.2f x + %.2f", r.m, r.q)
+}
+
+func (r1 Line) Intersection(r2 Line) (mpoint.Point, bool) {
+  if r1.m == r2.m  {
+    return mpoint.NewPoint(0,0), false
+  }
+  x := (r2.q - r1.q) / (r1.m - r2.m)
+  y := r1.m * x + r1.q
+  return mpoint.NewPoint(x,y), true
+}
+
+func (r Line) Dist(p mpoint.Point) float64 {
+  return math.Abs(r.q + r.m * p.X() - p.Y()) / math.Sqrt(1 + r.m * r.m)
 }
