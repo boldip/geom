6a93d2513cd07e0dff9543e6c8eba44214d9b8cb
Paolo Boldi <paolo.boldi@unimi.it>
2023-12-06 10:05:26 +0100

    Nuovo pacchetto per le rette

---
+++ mline/go.mod
@@ -0,0 +1,5 @@
+module github.com/boldip/geom/mline
+
+go 1.19
+
+require github.com/boldip/geom/mpoint v0.0.0-20231206090256-1af82d90802b
---
+++ mline/tutto.go
@@ -0,0 +1,23 @@
+package mline
+
+import (
+  "github.com/boldip/geom/mpoint"
+)
+
+/* A (straight) line specified by the equation y=mx+q */
+type Line struct {
+  m, q float64
+}
+
+func NewLine(m, q float64) (res Line) {
+  res.m,  res.q = m, q
+  return
+}
+
+func (r1 Line) IsParallel(r2 Line) bool {
+  return r1.m == r2.m
+}
+
+func (r Line) Belongs(mpoint.Point p) bool {
+  return p.Y() == r.m * p.X() + r.q
+}
